2f5370b70c596c778b3cc7357f672350dd253cf1
Heiko Ettwein <heiko.ettwein@gmx.de>
2019-01-09 09:12:38 +0100

    implementation with alternative concepts

--- evaluation.py
+++ evaluation.py
@@ -39,23 +39,36 @@ def draw_boxplot(m_col, p_col, l_col=None, method=None, horizon='', start=None, 
     
 def draw_boxplot_monthly(m_col, p_col, l_col, method, horizon):
     fig, ax = plt.subplots(figsize=(18, 18))
-    m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12 = '2016-02-01 00:00:00', '2016-03-01 00:00:00', '2016-04-01 00:00:00', '2016-05-01 00:00:00', '2016-06-01 00:00:00', '2016-07-01 00:00:00', '2016-08-01 00:00:00', '2016-09-01 00:00:00', '2016-10-01 00:00:00', '2016-11-01 00:00:00', '2016-12-01 00:00:00', '2017-01-01 00:00:00', '2017-02-01 00:00:00'
+    m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19, m20, m21, m22 = '2016-02-01 00:00:00', '2016-03-01 00:00:00', '2016-04-01 00:00:00', '2016-05-01 00:00:00', '2016-06-01 00:00:00', '2016-07-01 00:00:00', '2016-08-01 00:00:00', '2016-09-01 00:00:00', '2016-10-01 00:00:00', '2016-11-01 00:00:00', '2016-12-01 00:00:00', '2017-01-01 00:00:00', '2017-02-01 00:00:00', '2017-03-01 00:00:00', '2017-04-01 00:00:00', '2017-05-01 00:00:00', '2017-06-01 00:00:00', '2017-07-01 00:00:00', '2017-08-01 00:00:00', '2017-09-01 00:00:00', '2017-10-01 00:00:00', '2017-11-01 00:00:00', '2017-12-01 00:00:00'
     preds = np.array([p_col[m0:m1] - m_col[m0:m1], p_col[m1:m2] - m_col[m1:m2],
                       p_col[m2:m3] - m_col[m2:m3], p_col[m3:m4] - m_col[m3:m4],
                       p_col[m4:m5] - m_col[m4:m5], p_col[m5:m6] - m_col[m5:m6],
                       p_col[m6:m7] - m_col[m6:m7], p_col[m7:m8] - m_col[m7:m8],
                       p_col[m8:m9] - m_col[m8:m9], p_col[m9:m10] - m_col[m9:m10],
-                      p_col[m10:m11] - m_col[m10:m11], p_col[m11:m12] - m_col[m11:m12]])
+                      p_col[m10:m11] - m_col[m10:m11], p_col[m11:m12] - m_col[m11:m12],
+                      p_col[m12:m13] - m_col[m12:m13], p_col[m13:m14] - m_col[m13:m14],
+                      p_col[m14:m15] - m_col[m14:m15], p_col[m15:m16] - m_col[m15:m16],
+                      p_col[m16:m17] - m_col[m16:m17], p_col[m17:m18] - m_col[m17:m18],
+                      p_col[m18:m19] - m_col[m18:m19], p_col[m19:m20] - m_col[m19:m20],
+                      p_col[m20:m21] - m_col[m20:m21], p_col[m21:m22] - m_col[m21:m22],
+                      p_col[m22:] - m_col[m22:]])
     pvlibs = np.array([l_col[m0:m1] - m_col[m0:m1], l_col[m1:m2] - m_col[m1:m2],
                       l_col[m2:m3] - m_col[m2:m3], l_col[m3:m4] - m_col[m3:m4],
                       l_col[m4:m5] - m_col[m4:m5], l_col[m5:m6] - m_col[m5:m6],
                       l_col[m6:m7] - m_col[m6:m7], l_col[m7:m8] - m_col[m7:m8],
                       l_col[m8:m9] - m_col[m8:m9], l_col[m9:m10] - m_col[m9:m10],
-                      l_col[m10:m11] - m_col[m10:m11], l_col[m11:m12] - m_col[m11:m12]])
+                      l_col[m10:m11] - m_col[m10:m11], l_col[m11:m12] - m_col[m11:m12],
+                      l_col[m12:m13] - m_col[m12:m13], l_col[m13:m14] - m_col[m13:m14],
+                      l_col[m14:m15] - m_col[m14:m15], l_col[m15:m16] - m_col[m15:m16],
+                      l_col[m16:m17] - m_col[m16:m17], l_col[m17:m18] - m_col[m17:m18],
+                      l_col[m18:m19] - m_col[m18:m19], l_col[m19:m20] - m_col[m19:m20],
+                      l_col[m20:m21] - m_col[m20:m21], l_col[m21:m22] - m_col[m21:m22],
+                      l_col[m22:] - m_col[m22:]])
     bp1 = _draw_boxplot(preds, -0.2, ax, 'red', 'tomato', True, len(preds))
     bp2 = _draw_boxplot(pvlibs, 0.2, ax, 'blue', 'cornflowerblue', True, len(pvlibs))
     ax.legend([bp1["boxes"][0], bp2["boxes"][0]], [horizon, method])
-    ax.set_xticklabels(['february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december', 'january'])
+    ax.set_xticklabels(['february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'])
+    fig.autofmt_xdate()
     fig.savefig(dir + 'boxplot_' + horizon + '_vs_' + method + '_monthly.png', bbox_inches='tight')
     #fig.show()
     plt.close(fig)
@@ -144,8 +157,120 @@ def draw_history(history, test=False):
     #fig.show()
     plt.close(fig)
     
-def plot_error_by_hour_of_day(m_col, p_col, l_col, method):
-    hourly_err = []
+def plot_error_by_hour_of_day(data, method, shift, forecast_horizon):
+    data_h = pd.DataFrame(columns=['+'+str(i+1)+'h-prediction' for i in range(forecast_horizon)] + [method])
     for i in range(24):
-        for j in range(0, len(m_col), 24):
-            hourly_err.append(p_col[i + j] - m_col[i + j])
+        df = data.iloc[i::24]
+        l = []
+        for j in range(forecast_horizon):
+            l.append(math.sqrt(mean_squared_error(df.measured, df['+'+str(j+1)+'h-prediction'])))
+        l.append(math.sqrt(mean_squared_error(df.measured, df[method])))
+        data_h.loc[i] = l
+
+    for col in data_h.columns.values:
+        data_h[col] = np.roll(data_h[col], shift)
+        
+    fig, ax = plt.subplots(figsize=(12, 12))
+    ax.plot(data_h)
+    ax.set_title("mean RMSE per hour of day")
+    ax.set_ylabel("RMSE [watts]")
+    ax.set_yscale('linear')
+    ax.legend(data_h.columns.values)
+    fig.autofmt_xdate()
+    plt.grid(True)
+    fig.savefig(dir + 'mean_RMSE_per_hour.png')
+    #fig.show()
+    plt.close(fig)
+    
+def daily_energy_error(m_col, p_col, l_col, method, horizon, start=None, end=None):
+    j = int(len(m_col[start:end]) / 24)
+    dm = np.array_split(m_col[start:end], j)
+    dp = np.array_split(p_col[start:end], j)
+    dl = np.array_split(l_col[start:end], j)
+    
+    energy_m = []
+    energy_p = []
+    energy_l = []
+    abs_errors_p = []
+    abs_errors_l = []
+    errors_p = []
+    errors_l = []
+    for i in range(len(dm)):
+        m = np.trapz(dm[i])
+        p = np.trapz(dp[i])
+        l = np.trapz(dl[i])
+        abs_err_p = m - p
+        abs_err_l = m - l
+        abs_errors_p.append(abs_err_p)
+        abs_errors_l.append(abs_err_l)
+        errors_p.append(abs_err_p / m * 100)
+        errors_l.append(abs_err_l / m * 100)
+        energy_m.append(m)
+        energy_p.append(p)
+        energy_l.append(l)
+
+    print('daily mean ' + horizon + ' absolute energy error: ' + str(np.mean(abs_errors_p)))
+    print('daily mean ' + method + ' forecast absolute energy error: ' + str(np.mean(abs_errors_l)))
+    print('daily mean ' + horizon + ' relative energy error: ' + str(np.mean(errors_p)) + '%')
+    print('daily mean ' + method + ' forecast relative energy error: ' + str(np.mean(errors_l)) + '%')
+
+
+    fig, ax = plt.subplots(figsize=(18, 12))
+    ax.plot(abs_errors_p)
+    ax.plot(abs_errors_l)
+    ax.set_title("absolute daily energy error")
+    ax.set_ylabel("Energy [Wh]")
+    ax.set_yscale('linear')
+    ax.legend([horizon, method])
+    fig.autofmt_xdate()
+    plt.grid(True)
+    name = dir + horizon + '_abs_energy_error'
+    if start:
+        name += '_from' + start.replace(':', '-')
+    if end:
+        name += '_to' + end.replace(':', '-')
+    if l_col is not None:
+        name += '_with_' + method
+    fig.savefig(name + '.png')
+    #fig.show()
+    plt.close(fig)
+    
+    
+    fig, ax = plt.subplots(figsize=(18, 12))
+    ax.plot(energy_m)
+    ax.plot(energy_p)
+    #ax.plot(energy_l)
+    ax.set_title("daily energy")
+    ax.set_ylabel("Energy [Wh]")
+    ax.set_yscale('linear')
+    ax.legend(['measured', horizon])#, method])
+    fig.autofmt_xdate()
+    plt.grid(True)
+    name = dir + horizon + '_daily_energy'
+    if start:
+        name += '_from' + start.replace(':', '-')
+    if end:
+        name += '_to' + end.replace(':', '-')
+    #if l_col is not None:
+    #    name += '_with_' + method
+    fig.savefig(name + '.png')
+    #fig.show()
+    plt.close(fig)
+    
+    
+    fig, ax = plt.subplots(figsize=(12, 12))
+    bp1 = _draw_boxplot(errors_p, -0.2, ax, 'red', 'tomato')
+    #if l_col is not None:
+    #    bp2 = _draw_boxplot(errors_l, 0.2, ax, 'blue', 'cornflowerblue')
+    #    ax.legend([bp1["boxes"][0], bp2["boxes"][0]], [horizon, method])
+    #    name = dir + 'daily_mean_percentage_energy_error_' + horizon + '_vs_' + method
+    ax.legend([horizon + ' error'])
+
+    name = dir + horizon + '_daily_mean_percentage_energy_error'
+    if start:
+        name += '_from' + start.replace(':', '-')
+    if end:
+        name += '_to' + end.replace(':', '-')
+    fig.savefig(name + '.png', bbox_inches='tight')
+    #fig.show()
+    plt.close(fig)
--- machineLearningModel.py
+++ machineLearningModel.py
@@ -1,4 +1,4 @@
-from keras.layers import Input, Dense, Dropout, Flatten, LSTM, ConvLSTM2D, TimeDistributed
+from keras.layers import Input, Dense, Dropout, Flatten, LSTM, ConvLSTM2D, TimeDistributed, LeakyReLU, ReLU
 from keras.layers.convolutional import Conv1D, MaxPooling1D
 from keras.models import Model, Sequential
 from keras.utils import plot_model
@@ -37,14 +37,10 @@ class MultiLayerPerceptron(MachineLearnModel):
         for layer in range(0, num_layers):
             if dropout_rate > 0.0: 
                 pre = Dropout(dropout_rate)(pre)
-            pre = Dense(num_neurons, activation=act_fct)(pre)#, kernel_regularizer=l1_l2(0.1))(pre)
-        output = Dense(output_size, activation=out_act)(pre)
-#            pre = Dense(num_neurons, activation=act_fct,
-#                kernel_regularizer=regularizers.l2(0.0001),
-#                activity_regularizer=regularizers.l1(0.0001))(pre)
-#        output = Dense(output_size, activation=out_act,
-#                kernel_regularizer=regularizers.l2(0.0001),
-#                activity_regularizer=regularizers.l1(0.0001))(pre)
+            pre = Dense(num_neurons, activation=act_fct)(pre)
+        output = Dense(output_size)(pre)
+        output = LeakyReLU(alpha=0.001)(output)
+
         model = Model(inputs=visible, outputs=output)
         model.compile(loss=loss_fct, optimizer=optim, metrics=metrics)
         
@@ -59,9 +55,9 @@ class MultiLayerPerceptron(MachineLearnModel):
         self.verbose = verbose
         self.shuffle = shuffle
     
-    def learn(self, X, y):
-        return self.model.fit(X, y, self.batch_size, self.epochs, self.verbose, self.callbacks,
-                              self.validation_split, shuffle=self.shuffle)
+    def learn(self, X, y, val_idx=0):
+        return self.model.fit(X[val_idx:], y[val_idx:], self.batch_size, self.epochs, self.verbose, self.callbacks,
+                              validation_data=(X[:val_idx], y[:val_idx]), shuffle=self.shuffle)
     
     def forecast(self, X):
         return self.model.predict(X, self.batch_size, self.verbose)
@@ -80,7 +76,8 @@ class ConvolutionalNeuralNetwork(MachineLearnModel):
         pre = MaxPooling1D(pool_size=2)(pre)
         pre = Flatten()(pre)
         pre = Dense(num_neurons, activation=act_fct)(pre)
-        output = Dense(output_size, activation=out_act)(pre)
+        output = Dense(output_size)(pre)
+        output = LeakyReLU(alpha=0.001)(output)
         model = Model(inputs=visible, outputs=output)
         model.compile(loss=loss_fct, optimizer=optim, metrics=metrics)
         
@@ -119,7 +116,8 @@ class LongShortTermMemory(MachineLearnModel):
             model.add(LSTM(num_neurons, activation=act_fct))
         else:
             model.add(LSTM(num_neurons, activation=act_fct, input_shape=input_shape))
-        model.add(Dense(output_size, activation=out_act))
+        model.add(Dense(output_size))
+        model.add(LeakyReLU(alpha=0.001))
         model.compile(loss=loss_fct, optimizer=optim, metrics=metrics)
         
         if plot:
@@ -133,9 +131,9 @@ class LongShortTermMemory(MachineLearnModel):
         self.verbose = verbose
         self.shuffle = shuffle
     
-    def learn(self, X, y):
-        return self.model.fit(X, y, self.batch_size, self.epochs, self.verbose, self.callbacks,
-                              self.validation_split, shuffle=self.shuffle)
+    def learn(self, X, y, val_idx=0):
+        return self.model.fit(X[val_idx:], y[val_idx:], self.batch_size, self.epochs, self.verbose, self.callbacks,
+                              validation_data=(X[:val_idx], y[:val_idx]), shuffle=self.shuffle)
     
     def forecast(self, X):
         return self.model.predict(X, self.batch_size, self.verbose)
@@ -144,20 +142,21 @@ class LongShortTermMemory(MachineLearnModel):
     
 class DilatedConvolution(MachineLearnModel):
     
-    def __init__(self, input_shape, output_size, num_layers, loss_fct='mse', optim='adam',
+    def __init__(self, input_shape, output_size, num_layers, ks=3, fs=32, pa='causal', loss_fct='mse', optim='adam',
                  act_fct='relu', out_act='linear', metrics=[], dropout_rate=0, plot=None, batch_size=1,
                  epochs=1, validation_split=0.0, callbacks=None, verbose=1, shuffle=True, **kwargs):
         super().__init__('DilatedConvolution')
         
         model = Sequential()
-        model.add(Conv1D(256, 3, input_shape=input_shape, activation=act_fct, dilation_rate=3, padding="causal"))
-        model.add(Conv1D(128, 3, activation=act_fct, dilation_rate=3, padding="causal"))
-        model.add(Conv1D(64, 3, activation=act_fct, dilation_rate=3, padding="causal"))#'causal' 2 same
+        model.add(Conv1D(fs, ks, input_shape=input_shape, activation=act_fct, dilation_rate=1, padding="causal"))
+        for n in range(num_layers):
+            model.add(Conv1D(fs, ks, activation=act_fct, dilation_rate=2**(n+1), padding=pa))
         model.add(Flatten())
-        model.add(Dense(300, activation=act_fct))
-        model.add(Dense(300, activation=act_fct))
-        model.add(Dense(300, activation=act_fct))
-        model.add(Dense(output_size, activation=out_act))
+        model.add(Dense(50, activation=act_fct))
+        model.add(Dense(50, activation=act_fct))
+        model.add(Dense(50, activation=act_fct))
+        model.add(Dense(output_size))
+        model.add(LeakyReLU(alpha=0.001))
         model.compile(loss=loss_fct, optimizer=optim, metrics=metrics)
         
         if plot:
@@ -171,9 +170,9 @@ class DilatedConvolution(MachineLearnModel):
         self.verbose = verbose
         self.shuffle = shuffle
     
-    def learn(self, X, y):
-        return self.model.fit(X, y, self.batch_size, self.epochs, self.verbose, self.callbacks,
-                              self.validation_split, shuffle=self.shuffle)
+    def learn(self, X, y, val_idx=0):
+        return self.model.fit(X[val_idx:], y[val_idx:], self.batch_size, self.epochs, self.verbose, self.callbacks,
+                              validation_data=(X[:val_idx], y[:val_idx]), shuffle=self.shuffle)
     
     def forecast(self, X):
         return self.model.predict(X, self.batch_size, self.verbose)
@@ -192,7 +191,8 @@ class CNNLSTM(MachineLearnModel):
         model.add(TimeDistributed(MaxPooling1D(pool_size=2)))
         model.add(TimeDistributed(Flatten()))
         model.add(LSTM(num_neurons, activation=act_fct))
-        model.add(Dense(output_size, activation=out_act))
+        model.add(Dense(output_size))
+        model.add(LeakyReLU(alpha=0.001))
         model.compile(loss=loss_fct, optimizer=optim, metrics=metrics)
         
         if plot:
@@ -225,7 +225,8 @@ class ConvLSTM(MachineLearnModel):
         model = Sequential()
         model.add(ConvLSTM2D(filters=64, kernel_size=(1,2), activation=act_fct, input_shape=(1, 1, input_shape[0], input_shape[1])))#(n_seq, 1, n_steps, n_features))) #input_shape
         model.add(Flatten())
-        model.add(Dense(output_size, activation=out_act))
+        model.add(Dense(output_size))
+        model.add(LeakyReLU(alpha=0.001))
         model.compile(loss=loss_fct, optimizer=optim, metrics=metrics)
         
         if plot:
@@ -254,8 +255,9 @@ class RandomForest(MachineLearnModel):
         super().__init__('RandomForest')
         self.model = RandomForestRegressor(n_estimators=est, criterion=crit, oob_score=False, n_jobs=4, verbose=0, max_features=mf)#True
     
-    def learn(self, X, y):
-        return self.model.fit(X, y.ravel())
+    def learn(self, X, y, **kwargs):
+        return self.model.fit(X.values, y.values.ravel())
     
-    def forecast(self, X):
-        return self.model.predict(X)
+    def forecast(self, X, **kwargs):
+        return self.model.predict(X.values)
+        
